7162bb681f08a387b1fdbc2e6a6e10c2b50c8a83
RC4 <83184796+m7kra@users.noreply.github.com>
2022-10-19 11:14:13 +0100

    Fixed electronic configuration for some mobile browsers

--- periodic.js
+++ periodic.js
@@ -13,9 +13,31 @@ function display(element, mode) {
     document.querySelector('#period').innerHTML = mode != 'position'? period : '?';
 
     let configuration = element.electron_configuration_semantic;
-    configuration = configuration.replace(/(?<=[spdf])(\d+)/g, '<sup>$1</sup>');
+    // configuration = configuration.replace(/(?<=[spdf])(\d+)/g, '<sup>$1</sup>');
+    // the above line is not supported on some mobile browsers. Therefore, we
+    // have to resort to a more verbose solution, looping over the string:
+    let newConfiguration = '';
+    let i = 0;
+    while (i < configuration.length) {
+        if (configuration[i] == 's' || configuration[i] == 'p' || configuration[i] == 'd' || configuration[i] == 'f') {
+            newConfiguration += configuration[i];
+            i++;
+            if (i < configuration.length && configuration[i] >= '0' && configuration[i] <= '9') {
+                newConfiguration += '<sup>' + configuration[i]
+                i++;
+                if (i < configuration.length && configuration[i] >= '0' && configuration[i] <= '9') {
+                    newConfiguration += configuration[i];
+                    i++;
+                }
+                newConfiguration += '</sup>';
+            }
+        } else {
+            newConfiguration += configuration[i];
+            i++;
+        }
+    }
 
-    document.querySelector('#configuration').innerHTML = mode == 'show'? configuration : '?';
+    document.querySelector('#configuration').innerHTML = mode == 'show'? newConfiguration : '?';
 
     const button = document.querySelector('#button');
     if (mode != 'show') {
