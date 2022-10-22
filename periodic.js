4ae35753f28e4a14fe96f611bfa69e43cbdb2b90
m7kra <inboxaljezur@gmail.com>
2022-10-22 09:53:52 +0100

    Elements without cpk color no longer cause an error and changed element shape

--- periodic.js
+++ periodic.js
@@ -81,23 +81,21 @@ document.addEventListener('DOMContentLoaded', () => {
  * Get a contrasting color.
  * Code from https://stackoverflow.com/questions/635022/calculating-contrasting-colours-in-javascript/6511606
  */
- function contrastingColor(color)
- {
-     return (luma(color) >= 165) ? '0, 0, 0' : '255, 255, 255';
- }
- function luma(color) // color can be a hx string or an array of RGB values 0-255
- {
-     var rgb = (typeof color === 'string') ? hexToRGB(color) : color;
-     return (0.2126 * rgb[0]) + (0.7152 * rgb[1]) + (0.0722 * rgb[2]); // SMPTE C, Rec. 709 weightings
- }
- function hexToRGB(color)
- {
-     if (color.length === 3)
-         color = color.charAt(0) + color.charAt(0) + color.charAt(1) + color.charAt(1) + color.charAt(2) + color.charAt(2);
-     else if (color.length !== 6)
-         throw('Invalid hex color: ' + color);
-     var rgb = [];
-     for (var i = 0; i <= 2; i++)
-         rgb[i] = parseInt(color.substr(i * 2, 2), 16);
-     return rgb;
- }
+function contrastingColor(color) {
+    return (color && luma(color) >= 165) ? '0, 0, 0' : '255, 255, 255';
+}
+function luma(color) // color can be a hx string or an array of RGB values 0-255
+{
+    var rgb = (typeof color === 'string') ? hexToRGB(color) : color;
+    return (0.2126 * rgb[0]) + (0.7152 * rgb[1]) + (0.0722 * rgb[2]); // SMPTE C, Rec. 709 weightings
+}
+function hexToRGB(color) {
+    if (color.length === 3)
+        color = color.charAt(0) + color.charAt(0) + color.charAt(1) + color.charAt(1) + color.charAt(2) + color.charAt(2);
+    else if (color.length !== 6)
+        throw ('Invalid hex color: ' + color);
+    var rgb = [];
+    for (var i = 0; i <= 2; i++)
+        rgb[i] = parseInt(color.substr(i * 2, 2), 16);
+    return rgb;
+}
