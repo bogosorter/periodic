207e3cb9a6647fb4726bd25b957212921b2ef287
m7kra <inboxaljezur@gmail.com>
2022-10-22 09:46:25 +0100

    Changed colors

--- periodic.js
+++ periodic.js
@@ -23,6 +23,13 @@ function display(element, mode) {
 
     document.querySelector('#configuration').innerHTML = mode == 'show'? configuration : '?';
 
+    // Change colors according to cpk-hex
+    const bgColor = hexToRGB(element['cpk-hex']);
+    const color = contrastingColor(bgColor);
+    document.querySelector('#element').style.setProperty('--bg-color', bgColor.join(', '));
+    document.querySelector('#element').style.setProperty('color', `rgb(${color})`);
+
+
     const button = document.querySelector('#button');
     if (mode != 'show') {
         button.innerHTML = 'Show';
@@ -53,3 +60,28 @@ document.addEventListener('DOMContentLoaded', () => {
     // Initialize the content
     next();
 })
+
+/**
+ * Get a contrasting color.
+ * Code from https://stackoverflow.com/questions/635022/calculating-contrasting-colours-in-javascript/6511606
+ */
+ function contrastingColor(color)
+ {
+     return (luma(color) >= 165) ? '0, 0, 0' : '255, 255, 255';
+ }
+ function luma(color) // color can be a hx string or an array of RGB values 0-255
+ {
+     var rgb = (typeof color === 'string') ? hexToRGB(color) : color;
+     return (0.2126 * rgb[0]) + (0.7152 * rgb[1]) + (0.0722 * rgb[2]); // SMPTE C, Rec. 709 weightings
+ }
+ function hexToRGB(color)
+ {
+     if (color.length === 3)
+         color = color.charAt(0) + color.charAt(0) + color.charAt(1) + color.charAt(1) + color.charAt(2) + color.charAt(2);
+     else if (color.length !== 6)
+         throw('Invalid hex color: ' + color);
+     var rgb = [];
+     for (var i = 0; i <= 2; i++)
+         rgb[i] = parseInt(color.substr(i * 2, 2), 16);
+     return rgb;
+ }
